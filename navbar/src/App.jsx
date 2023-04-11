f7b20927d45f6aa97137194aca14fd41208f5675
erikjearl <erikjearl@gmail.com>
2023-04-11 16:39:49 -0400

    navbar start

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Navbar>
+      <NavItem icon="👀" />
+      <NavItem icon="❤️" />
+      <NavItem icon="🤠" />
+    </Navbar>
+  )
+}
+
+function Navbar(props) {
+  return(
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        { props.children }
+      </ul>
+    </nav>
+  )
+}
+function NavItem(props) {
+  return(
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
   )
 }
 
