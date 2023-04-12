51859cd0ca332f2b647274c4b353eb882349642a
erikjearl <erikjearl@gmail.com>
2023-04-12 17:35:33 -0400

    navbar
    bugs

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -1,13 +1,81 @@
-import { useState } from 'react'
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+
+import React, { useState, useEffect, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 function App() {
-
   return (
     <Navbar>
-      <NavItem icon="👀" />
-      <NavItem icon="❤️" />
-      <NavItem icon="🤠" />
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu />
+      </NavItem>
     </Navbar>
+  );
+}
+
+function DropdownMenu() {
+
+  const [activeMenu, setActiveMenu] = useState('main'); // settings
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item" onClick={()=> props.goToMenu && setActiveMenu(props.goToMenu)}>
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return(
+    <div className="dropdown">
+
+      {/* Dropdown Menu */}
+      <CSSTransition 
+      in={activeMenu === 'main'} 
+      unmountOnExit 
+      timeout={500}
+      classNames="menu-primary"
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem 
+            leftIcon={<CogIcon />}  
+            rightIcon={<ChevronIcon 
+            goToMenu="settings" />} 
+          />
+        </div>
+      </CSSTransition>
+
+      {/* Settings Menu */}
+      <CSSTransition 
+      in={activeMenu === 'settings'} 
+      unmountOnExit 
+      timeout={500}
+      classNames="menu-secondary"
+      >
+        <div className="menu">
+          <DropdownItem>My Settings</DropdownItem>
+          <DropdownItem 
+            leftIcon={<CogIcon />} 
+            rightIcon={<ChevronIcon
+            goToMenu="settings" />}
+          />
+        </div>
+      </CSSTransition>
+
+    </div>
   )
 }
 
@@ -21,11 +89,15 @@ function Navbar(props) {
   )
 }
 function NavItem(props) {
+  
+  const [open, setOpen] = useState(false);
+
   return(
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+      {open && props.children}
     </li>
   )
 }
